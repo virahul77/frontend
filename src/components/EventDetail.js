ba8b235e70f89a00bbb3db6956024d1a922015d7
Rahul Vishwakarma <vishwakarma.rahul77@gmail.com>
2022-12-30 11:16:52 +0530

    Added details page fn

--- src/components/EventDetail.js
+++ src/components/EventDetail.js
@@ -3,6 +3,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate, useParams } from 'react-router-dom'
 import { backendUrl } from '../App';
 import NavBar from './NavBar'
+import PartEventDetails from './PartEventDetails';
 
 const EventDetail = () => {
   const params = useParams();
@@ -26,7 +27,9 @@ const EventDetail = () => {
   return (
     <div>
         <NavBar />
-        Single Event
+        <div className="container mt-3">
+          <PartEventDetails eventId={eventId}/>
+        </div>
     </div>
   )
 }
--- src/components/PartEventDetails.js
+++ src/components/PartEventDetails.js
@@ -5,6 +5,7 @@ import { backendUrl } from '../App';
 
 const PartEventDetails = ({eventId}) => {
   const [event,setEvent] = useState({});
+  let isPast = new Date(event.startDate)<Date.now();
   const token = useSelector(state=>state.token);
   const navigate = useNavigate();
   const getEventDetail = async()=> {
@@ -22,8 +23,35 @@ const PartEventDetails = ({eventId}) => {
     getEventDetail();
   },[])
   return (
-    <div>
-        Participated Event Detail
+    <div className='mb-4'>
+      <div className='card w-60' style={{overflow:'hidden'}}>
+        <img src={event.image} alt="event pic" style={{height:'500px'}} />
+        <div className='card-body'>
+          <h5 className="card-title">{event.eventName}</h5>
+          <p>Created By :- {event.createdBy?.username}</p>
+
+          {!isPast && <p>Start Date: {new Date(event.startDate).toLocaleDateString()}</p>}
+          {isPast && <p>Event Finished</p>}
+          {!isPast && <p>Total Seats : {event.totalSeats}</p>}
+          {!isPast && <p>Available Seats : {event.totalSeats - event.currentParticipants?.length}</p>}
+          <p>Description : {event.description}</p>
+          
+          {event.participated && <div>
+            <p>All Participants</p>
+            <ul className="list-group">
+                {event?.currentParticipants?.map(user=>{
+                    return (
+                        <div className="input-group mb-3" key={user._id}>
+                            <span type="text" className="form-control" value={user.username}>{user.username}</span>
+                        </div>
+                    )
+                })}
+                {event?.currentParticipants?.length===0 && <p>No Participants Joined Yet</p>}
+            </ul>
+          </div>}
+          {isPast && <button className='card-link btn-outline-success btn disabled'>Finished</button>}
+        </div>
+        </div>
     </div>
   )
 }
